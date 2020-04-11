a821e6607a6c2663a78874e4ce70f35b47dfddde
Erik Donohoo <erikdonohoo@gmail.com>
2020-04-10 20:03:03 -0600

    Finish site, add temp contact form

---
+++ script.js
@@ -0,0 +1,37 @@
+(() => {
+  // Prevent form from being submitted if its not filled out
+  const form = document.querySelector('form');
+  const email = document.querySelector('input#email');
+  const name = document.querySelector('input#name');
+  const message = document.querySelector('textarea#message');
+
+  let isInvalid = true, isSubmitted = false;
+
+  const handleChange = () => {
+    if (!isInvalid) {
+      form.classList.remove('error');
+    }
+
+    if (isInvalid && isSubmitted) {
+      form.classList.add('error');
+    }
+  };
+
+  const onInput = () => {
+    isInvalid = !email.value && !name.value;
+    handleChange();
+  };
+
+  email.addEventListener('keyup', onInput);
+  name.addEventListener('keyup', onInput);
+  message.addEventListener('keyup', onInput);
+
+  form.addEventListener('submit', (event) => {
+    isSubmitted = true;
+    if (!email.value || !name.value) {
+      event.preventDefault();
+    }
+    handleChange();
+  });
+
+})();
